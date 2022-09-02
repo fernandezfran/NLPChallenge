974ef41594033cbf8a12bd3fb36ba74cbb75e6e4
fernandezfran <fernandezfrancisco2195@gmail.com>
2022-09-02 10:41:10 -0300

    point 2 done

--- natural_language_processing.py
+++ natural_language_processing.py
@@ -48,15 +48,28 @@ def bag_of_words_model(corpus):
     return cv
 
 
-def try_models(X_train, X_test, y_train, y_test, models):
+def train_models(X_train, X_test, y_train, y_test, models, models_names, verbose=1):
     """Try different classification models."""
-    for classifier in models:
+    if verbose:
+        print("model,accuracy,precision,recall,f1score")
+
+    accuracy = []
+    for classifier, name in zip(models, models_names):
         classifier.fit(X_train, y_train)
         y_pred = classifier.predict(X_test)
 
-        cm = sklearn.metrics.confusion_matrix(y_test, y_pred)
-        accuracy = sklearn.metrics.accuracy_score(y_test, y_pred)
-        print(cm, accuracy)
+        acc = sklearn.metrics.accuracy_score(y_test, y_pred)
+
+        prec, rec, fscor, sup = sklearn.metrics.precision_recall_fscore_support(
+            y_test, y_pred, average="macro"
+        )
+
+        if verbose:
+            print(f"{name},{acc},{prec},{rec},{fscor}")
+
+        accuracy.append(acc)
+
+    return accuracy
 
 
 def main():
@@ -77,6 +90,15 @@ def main():
     )
 
     # Different models
+    classifiers_names = (
+        "Logistic Regression",
+        "KNN",
+        "SVM linear",
+        "SVM rbf",
+        "Naive Bayes",
+        "Decision Tree",
+        "Random Forest",
+    )
     classifiers = (
         sklearn.linear_model.LogisticRegression(random_state=0),
         sklearn.neighbors.KNeighborsClassifier(),
@@ -86,8 +108,22 @@ def main():
         sklearn.tree.DecisionTreeClassifier(criterion="entropy", random_state=0),
         sklearn.ensemble.RandomForestClassifier(criterion="entropy", random_state=0),
     )
+    accuracy = train_models(
+        X_train, X_test, y_train, y_test, classifiers, classifiers_names
+    )
 
-    try_models(X_train, X_test, y_train, y_test, classifiers)
+    # Plot accuracy
+    fig, ax = plt.subplots()
+
+    x = np.arange(0, len(classifiers))
+
+    ax.bar(x, accuracy)
+
+    ax.set_xticks(x, classifiers_names, rotation=45)
+    ax.set_ylabel("Accuracy")
+
+    fig.tight_layout()
+    fig.savefig("models_accuracy.png", dpi=600)
 
 
 if __name__ == "__main__":
